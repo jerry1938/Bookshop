92dc558b466611e9c5f98922c873e8cffaae8aad
jerry5585 <jerry5585@yahoo.com>
2021-03-31 19:04:44 +0200

    Created the AdminHomeController

---
+++ Bookshop/Controllers/AdminHomeController.cs
@@ -0,0 +1,40 @@
+﻿using Bookshop.Views.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.Controllers
+{
+    class AdminHomeController
+    {
+        Layout layout = new Layout();
+        MenuController menuController = new MenuController();
+
+        public void Index()
+        {
+            layout.ClearMainContent();
+            layout.ClearMenu();
+
+            int option = menuController.Menu(Views.AdminHome.Index.MenuOptions);
+            switch (option)
+            {
+                case 0: // Users
+                    AdminUsersController adminUsersController = new AdminUsersController();
+                    adminUsersController.Index();
+                    break;
+                case 1: // Books
+                    break;
+                case 2: // Statistics
+                    break;
+                case 3: // Logout
+                    HomeController homeController = new HomeController();
+                    homeController.Logout();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
