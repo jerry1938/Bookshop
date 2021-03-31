78b4bbd4a24817f98c4a39e5f0e52f91ba9fc7ef
jerry5585 <jerry5585@yahoo.com>
2021-03-31 20:52:42 +0200

    Created the view for listUsers

---
+++ Bookshop/Views/AdminUsers/ListUsers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.Views.AdminUsers
+{
+    public static class ListUsers
+    {
+        public static List<string> MenuOptions = new List<string>() { "Add user", "Find user",
+            "Activate user", "Inactivate user", "Promote user", "Demote user", "Back"};
+
+        public static void ListAllUsers(List<Webbutik.Models.User> listOfUsers)
+        {
+            Console.SetCursorPosition(27, 9);
+            foreach (var item in listOfUsers)
+            {
+                Console.Write(item.Name);
+                Console.CursorLeft = 55;
+                Console.Write(item.IsAdmin);
+                Console.CursorLeft = 80;
+                Console.WriteLine(item.IsActive);
+                Console.CursorLeft = 27;
+            }
+        }
+    }
+}
