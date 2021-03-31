fd1b9a8c17939938f4a386e94a87267c92f448af
jerry5585 <jerry5585@yahoo.com>
2021-03-31 19:07:09 +0200

    Created the AdminHome view

---
+++ Bookshop/Views/AdminHome/Index.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookshop.Views.AdminHome
+{
+    public static class Index
+    {
+        public static List<string> MenuOptions = new List<string>() { "Users", "Books",
+            "Statistics", "Logout"};
+    }
+}
