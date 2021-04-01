13d035174292a6b6055ba6f7f5910593e2d9b76e
jerry5585 <jerry5585@yahoo.com>
2021-04-01 17:49:08 +0200

    Finished the demote method

--- Bookshop/Controllers/AdminUsersController.cs
+++ Bookshop/Controllers/AdminUsersController.cs
@@ -150,6 +150,7 @@ namespace Bookshop.Controllers
                     PromoteUser(userId);
                     break;
                 case 3: // Demote user
+                    DemoteUser(userId);
                     break;
                 case 4: // Back
                     ListUsers();
@@ -216,7 +217,7 @@ namespace Bookshop.Controllers
             layout.ClearMenu();
             
             Views.AdminUsers.ActivateUser.Confirm();
-            menu.PrintMessageBox(0);
+            menu.PrintMessageBox(1);
 
             int option = menuController.Menu(Views.AdminUsers.ActivateUser.MenuOptions);
             switch (option)
@@ -237,6 +238,7 @@ namespace Bookshop.Controllers
                     PromoteUser(userId);
                     break;
                 case 5: // Demote user
+                    DemoteUser(userId);
                     break;
                 case 6: // Back
                     Index();
@@ -269,7 +271,7 @@ namespace Bookshop.Controllers
             layout.ClearMenu();
 
             Views.AdminUsers.InactivateUser.Confirm();
-            menu.PrintMessageBox(0);
+            menu.PrintMessageBox(1);
 
             int option = menuController.Menu(Views.AdminUsers.InactivateUser.MenuOptions);
             switch (option)
@@ -290,6 +292,7 @@ namespace Bookshop.Controllers
                     PromoteUser(userId);
                     break;
                 case 5: // Demote user
+                    DemoteUser(userId);
                     break;
                 case 6: // Back
                     Index();
@@ -322,7 +325,7 @@ namespace Bookshop.Controllers
             layout.ClearMenu();
 
             Views.AdminUsers.PromoteUser.Confirm();
-            menu.PrintMessageBox(0);
+            menu.PrintMessageBox(1);
 
             int option = menuController.Menu(Views.AdminUsers.PromoteUser.MenuOptions);
             switch (option)
@@ -343,6 +346,7 @@ namespace Bookshop.Controllers
                     InactivateUser(userId);
                     break;
                 case 5: // Demote user
+                    DemoteUser(userId);
                     break;
                 case 6: // Back
                     Index();
@@ -357,6 +361,61 @@ namespace Bookshop.Controllers
                 case 0:
                     bool isPromoted = GlobalVariables.Api.Promote(GlobalVariables.User.Id, userId);
                     Views.AdminUsers.PromoteUser.IsUserPromoted(isPromoted);
+                    Index();
+                    break;
+                case 1:
+                    Index();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void DemoteUser(int userId)
+        {
+            Menu menu = new Menu();
+
+            layout.ClearMainContent();
+            layout.ClearMenu();
+
+            Views.AdminUsers.DemoteUser.Confirm();
+            menu.PrintMessageBox(1);
+
+            int option = menuController.Menu(Views.AdminUsers.DemoteUser.MenuOptions);
+            switch (option)
+            {
+                case 0: // Add user
+                    AddUser();
+                    break;
+                case 1: // List users
+                    ListUsers();
+                    break;
+                case 2: // Find user
+                    FindUser();
+                    break;
+                case 3: // Activate user
+                    ActivateUser(userId);
+                    break;
+                case 4: // Inactivate user
+                    InactivateUser(userId);
+                    break;
+                case 5: // Promote user
+                    PromoteUser(userId);
+                    break;
+                case 6: // Back
+                    Index();
+                    break;
+                default:
+                    break;
+            }
+
+            option = menuController.MessageWindow();
+            switch (option)
+            {
+                case 0:
+                    bool isPromoted = GlobalVariables.Api.Demote(GlobalVariables.User.Id, userId);
+                    Views.AdminUsers.DemoteUser.IsUserDemoted(isPromoted);
+                    Index();
                     break;
                 case 1:
                     Index();
